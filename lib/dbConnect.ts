dcfca960263cfd20a8e4a885f1911dfc96104f88
osvaldosurbakti <osvaldosurbakti@gmail.com>
2025-11-12 20:03:04 +0700

    redeploy1

--- lib/dbConnect.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectToDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
-  return mongoose.connect(process.env.MONGODB_URI as string);
-};
